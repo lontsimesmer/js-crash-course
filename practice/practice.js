29bdcd3b7fedda7067ee80b4e1111dc1c267f947
lontsimesmer <tchoupounathanael@gmail.com>
2023-12-09 21:26:26 +0100

    add pratice on variables and constants

--- practice/practice.js
+++ practice/practice.js
@@ -1,16 +1,26 @@
 // Variable declaration using let
-const result = 2
+const result = 6;
 
-declareVariables = (result) => {
+declareVariables = (_result) => {
+  console.log(result); // 6
   let x;
   console.log(x); // undefined
   let y = 2;
   console.log(y); // 2
 
   // Variables naming
+
   let $a = "Jesus";
-  console.log($a); // This is valid
+  console.log($a); // Jesus
   let _b = "Holly";
-  console.log(_b);
+  console.log(_b); // Holly
+
+  // Js Keywords can't be variable names
+//   let type = "object";
+//   const name = new type(arguments);
+//   console.log(name); // The keyword here is new
+
+//   const array = Array.fill(0);
+//   console.log(array); // The keyword here is Array.fill
 };
 declareVariables(result);
