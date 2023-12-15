3f9e22e3db85dc27fee23dae0a80b72239948d16
lontsimesmer <tchoupounathanael@gmail.com>
2023-12-15 23:58:47 +0100

    add number data type

--- practice/practice.js
+++ practice/practice.js
@@ -1,50 +1,52 @@
 // Variable declaration using let
 
-const result = 6
+const result = 6;
 declareVariables = (_result) => {
-  console.log(result) // 6
-  let x
-  console.log(x) // undefined
-  const y = 2
-  console.log(y) // 2
+  console.log(result); // 6
+  let x;
+  console.log(x); // undefined
+  const y = 2;
+  console.log(y); // 2
 
   // Variables naming
-  const $a = 'Jesus'
-  console.log($a) // Jesus
-  const _b = 'Holly'
-  console.log(_b) // Holly
-}
-declareVariables(result)
-
+  const $a = "Jesus";
+  console.log($a); // Jesus
+  const _b = "Holly";
+  console.log(_b); // Holly
+};
+declareVariables(result);
 
 // Const declaration using const
 
-const result1 = 7
+const result1 = 7;
 declareConstants = (_result1) => {
-  console.log(result1)
-  const x = 5
-  console.log(x)
-}
-declareConstants(result1)
-
+  console.log(result1);
+  const x = 5;
+  console.log(x);
+};
+declareConstants(result1);
 
 // JS data types
 
-const result2 = 8
+const result2 = 8;
 dataTypes = (_result2) => {
-
   // Strings
-  const name = 'Lontsi'
-  const surname = 'Mesmer'
-  const result = `The names are ${name} and ${surname}`
+  const name = "Lontsi";
+  const surname = "Mesmer";
+  const result = `The names are ${name} and ${surname}`;
   console.log(result);
 
   // Number
-  const number = 3e5
+  const number = 3e5;
   console.log(number);
-  const number1 = 3/0
+  const number1 = 3 / 0;
   console.log(number1);
-  const number2 = 'abc'/3
+  const number2 = "abc" / 3;
   console.log(number2);
-}
-dataTypes(result2)
+
+  // BigInt
+  const value = 900719925124740998n;
+  const result1 = value + 1n;
+  console.log(result1);
+};
+dataTypes(result2);
