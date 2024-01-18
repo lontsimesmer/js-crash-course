167c99a4ee1baddb481740cacee22acc817283ec
lontsimesmer <tchoupounathanael@gmail.com>
2024-01-18 17:00:12 +0100

    done standard fix

--- practice/practice.js
+++ practice/practice.js
@@ -1,179 +1,179 @@
 // Variable declaration using let
 
-const result = 6;
+const result = 6
 declareVariables = (_result) => {
-  let x;
-  console.log(x); // undefined
-  const y = 2;
-  console.log(y); // 2
+  let x
+  console.log(x) // undefined
+  const y = 2
+  console.log(y) // 2
 
   // Variables naming
-  const $a = "Jesus";
-  console.log($a); // Jesus
-  const _b = "Holly";
-  console.log(_b); // Holly
-};
-declareVariables(result);
+  const $a = 'Jesus'
+  console.log($a) // Jesus
+  const _b = 'Holly'
+  console.log(_b) // Holly
+}
+declareVariables(result)
 
 // Constants declaration using const
 
-const result1 = 7;
+const result1 = 7
 declareConstants = (_result1) => {
-  console.log(result1);
-  const x = 5;
-  console.log(x);
-};
-declareConstants(result1);
+  console.log(result1)
+  const x = 5
+  console.log(x)
+}
+declareConstants(result1)
 
 // JS data types
 
-const result2 = 8;
+const result2 = 8
 dataTypes = (_result2) => {
   // Strings
-  const name = "Lontsi";
-  const surname = "Mesmer";
-  const result = `The names are ${name} and ${surname}`;
-  console.log(result);
+  const name = 'Lontsi'
+  const surname = 'Mesmer'
+  const result = `The names are ${name} and ${surname}`
+  console.log(result)
 
   // Number
-  const number = 3e5;
-  console.log(number);
-  const number1 = 3 / 0;
-  console.log(number1);
-  const number2 = "abc" / 3;
-  console.log(number2);
+  const number = 3e5
+  console.log(number)
+  const number1 = 3 / 0
+  console.log(number1)
+  const number2 = 'abc' / 3
+  console.log(number2)
 
   // BigInt
-  const value = 900719925124740998n;
-  const result1 = value + 1n;
-  console.log(result1);
+  const value = 900719925124740998n
+  const result1 = value + 1n
+  console.log(result1)
 
   // undefined
-  let age;
-  console.log(age);
-  const result2 = undefined;
-  console.log(result2);
+  let age
+  console.log(age)
+  const result2 = undefined
+  console.log(result2)
 
   // Typeof operator
-  const firstName = "Lontsi";
-  console.log(typeof firstName);
-  const _class = 1;
-  console.log(typeof _class);
-  const a = null;
-  console.log(typeof a);
-};
-dataTypes(result2);
+  const firstName = 'Lontsi'
+  console.log(typeof firstName)
+  const _class = 1
+  console.log(typeof _class)
+  const a = null
+  console.log(typeof a)
+}
+dataTypes(result2)
 
 // Operators in javascript
 
-const result3 = 9;
+const result3 = 9
 writeOperations = (_result3) => {
   // Arithmetic
-  let x = 2;
-  let y = 3;
-  const add = x + y;
-  console.log("x + y =", add);
-  const minus = x - y;
-  console.log("x - y =", minus);
-  const multiple = x * y;
-  console.log("x * y =", multiple);
-  const divide = x / y;
-  console.log("x / y =", divide);
-  const remainder = x % y;
-  console.log("x % y =", remainder);
-  const increase = ++x;
-  console.log("++x =", increase);
-  console.log("++x =", x++);
-  const decrease = --y;
-  console.log("--y =", decrease);
-  console.log("--y =", y--);
-  const exponent = x ** y;
-  console.log(exponent);
+  let x = 2
+  let y = 3
+  const add = x + y
+  console.log('x + y =', add)
+  const minus = x - y
+  console.log('x - y =', minus)
+  const multiple = x * y
+  console.log('x * y =', multiple)
+  const divide = x / y
+  console.log('x / y =', divide)
+  const remainder = x % y
+  console.log('x % y =', remainder)
+  const increase = ++x
+  console.log('++x =', increase)
+  console.log('++x =', x++)
+  const decrease = --y
+  console.log('--y =', decrease)
+  console.log('--y =', y--)
+  const exponent = x ** y
+  console.log(exponent)
 
   // Comparison
-  const a = 1;
-  const b = 2;
-  const c = 3;
-  console.log(a == a);
-  console.log(2 == "2");
-  console.log(a == "a");
-  console.log(a != b);
-  console.log(b != c);
-  console.log(b === b);
-  console.log(b === "b");
-  console.log(c !== "c");
-  console.log(c !== c);
-  console.log(a > b);
+  const a = 1
+  const b = 2
+  const c = 3
+  console.log(a == a)
+  console.log(2 == '2')
+  console.log(a == 'a')
+  console.log(a != b)
+  console.log(b != c)
+  console.log(b === b)
+  console.log(b === 'b')
+  console.log(c !== 'c')
+  console.log(c !== c)
+  console.log(a > b)
 
   // Logical
-  const m = 5;
-  const n = 4;
-  const andLogic = m > n && n < m;
-  console.log(andLogic); // true
-  console.log(m > 4 && n < 5);
-  console.log(true && false); // false
-  const orLogic = n < m || n > m;
-  console.log(orLogic); // true
-  console.log(n < 6 || m > 7);
-  console.log(false || false); // false
-  const notLogic = !true;
-  console.log(notLogic); // false
-  console.log(!false); // true
-  console.log(true && false); // false
-  console.log(true && true); // true
-  console.log(!true || !false); // true
+  const m = 5
+  const n = 4
+  const andLogic = m > n && n < m
+  console.log(andLogic) // true
+  console.log(m > 4 && n < 5)
+  console.log(true && false) // false
+  const orLogic = n < m || n > m
+  console.log(orLogic) // true
+  console.log(n < 6 || m > 7)
+  console.log(false || false) // false
+  const notLogic = !true
+  console.log(notLogic) // false
+  console.log(!false) // true
+  console.log(true && false) // false
+  console.log(true && true) // true
+  console.log(!true || !false) // true
 
   // String operators
-  console.log("hello" + " " + "world");
-  let value = "JavaScript";
-  console.log((value += "tutorial"));
-};
-writeOperations(result3);
+  console.log('hello' + ' ' + 'world')
+  let value = 'JavaScript'
+  console.log((value += 'tutorial'))
+}
+writeOperations(result3)
 
 // Type Conversion
 
-const result4 = 10;
+const result4 = 10
 typeConvert = (_result4) => {
   // Implicit conversion
-  const value = "2" + 5; // "25"
-  console.log(value);
-  const value1 = "3" + true;
-  console.log(value1); // "3true"
+  const value = '2' + 5 // "25"
+  console.log(value)
+  const value1 = '3' + true
+  console.log(value1) // "3true"
 
-  const value2 = "4" - "2";
-  console.log(value2); // 2
-  const value3 = "4" * "2";
-  console.log(value3); // 8
-  const value4 = "4" / "2";
-  console.log(value4);
+  const value2 = '4' - '2'
+  console.log(value2) // 2
+  const value3 = '4' * '2'
+  console.log(value3) // 8
+  const value4 = '4' / '2'
+  console.log(value4)
 
-  const res = "hello" - "world";
-  console.log(res); // NaN
-  const res1 = "4" - "Hello";
-  console.log(res1); // NaN
-  const res2 = "4" - true;
-  console.log(res2); // 3
-  const res3 = "4" - false;
-  console.log(res3); // 4
-  const res4 = null + undefined;
-  console.log(res4);
+  const res = 'hello' - 'world'
+  console.log(res) // NaN
+  const res1 = '4' - 'Hello'
+  console.log(res1) // NaN
+  const res2 = '4' - true
+  console.log(res2) // 3
+  const res3 = '4' - false
+  console.log(res3) // 4
+  const res4 = null + undefined
+  console.log(res4)
 
   // Explicit conversion
-  const res5 = Number("237e-1");
-  console.log(res5);
-  const res6 = Number("");
-  console.log(res6); // 0
+  const res5 = Number('237e-1')
+  console.log(res5)
+  const res6 = Number('')
+  console.log(res6) // 0
 
-  const num = Number(null);
-  console.log(num); // 0
-  const num1 = parseInt("20.01");
-  console.log(num1);
-  const num2 = Math.floor("25.9");
-  console.log(num2); // 25
+  const num = Number(null)
+  console.log(num) // 0
+  const num1 = parseInt('20.01')
+  console.log(num1)
+  const num2 = Math.floor('25.9')
+  console.log(num2) // 25
 
-  const str = String(237);
-  console.log(str);
-  const str1 = (237).toString();
-  console.log(str1);
-};
-typeConvert(result4);
+  const str = String(237)
+  console.log(str)
+  const str1 = (237).toString()
+  console.log(str1)
+}
+typeConvert(result4)
--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 // const e = 0
 
 // assigment
-const c = "name";
-console.log(c);
+const c = 'name'
+console.log(c)
 
 //  primitives datatypes
 
@@ -16,20 +16,20 @@ console.log(c);
 // const isDorineAngryAtheJSTeacher = true
 
 // String
-const student1 = "Dorren";
-const student2 = "Tally";
-const mondayTeacher = `Emma will teach ${student1} and ${student2}`;
-console.log(mondayTeacher);
+const student1 = 'Dorren'
+const student2 = 'Tally'
+const mondayTeacher = `Emma will teach ${student1} and ${student2}`
+console.log(mondayTeacher)
 
 //  complex datatype
 
 // Arrays
-const arr1 = [];
+const arr1 = []
 // const arr2 = [4] // arrays with 4 spaces empty
-arr1[3] = 5; // in position 3 put the value 5
-arr1.push(4); // in a la fin du array ajoute 4
-arr1.pop(); // ca va montrer le denier index en le retirant
-arr1.at(2); // ca va te donner la position de ta valeur en le laissant
+arr1[3] = 5 // in position 3 put the value 5
+arr1.push(4) // in a la fin du array ajoute 4
+arr1.pop() // ca va montrer le denier index en le retirant
+arr1.at(2) // ca va te donner la position de ta valeur en le laissant
 
 // Set is like an array
 
@@ -37,18 +37,18 @@ arr1.at(2); // ca va te donner la position de ta valeur en le laissant
 
 // Objects
 const teachers = {
-  javascript: "Carlson",
-  react: "Giles",
-  node: "Leonard",
-};
-console.log({ ...teachers }); // print teachers
-const profs = teachers;
-profs.javascript = "jules"; // Carleson = jules
-console.log({ ...profs }, { ...teachers }); // la var profs aura les meme propr de teacher et les deux changerons leur noms javascript en jules
-const role = teachers.javascript; // to get a value of an object
-console.log(role);
-const role1 = teachers.javascript; // to get a value of an object
-console.log(role1);
+  javascript: 'Carlson',
+  react: 'Giles',
+  node: 'Leonard'
+}
+console.log({ ...teachers }) // print teachers
+const profs = teachers
+profs.javascript = 'jules' // Carleson = jules
+console.log({ ...profs }, { ...teachers }) // la var profs aura les meme propr de teacher et les deux changerons leur noms javascript en jules
+const role = teachers.javascript // to get a value of an object
+console.log(role)
+const role1 = teachers.javascript // to get a value of an object
+console.log(role1)
 
 // conditional logic
 
@@ -58,75 +58,76 @@ console.log(role1);
 //   return `${new Date().toUTCString()} ${status}  ${message} `;
 // }
 
-const log = (message, status = "error") =>
-  `${new Date().toUTCString()} ${status}  ${message} `; // Anonyms function
-log();
+const log = (message, status = 'error') =>
+  `${new Date().toUTCString()} ${status}  ${message} ` // Anonyms function
+log()
 
-function craeteLogger() {
+function craeteLogger () {
   // higher order function (function that return another function)
-  function writeToConsole(message, status) {
+  function writeToConsole (message, status) {
     // console.log(log(a, b));
   }
-  return writeToConsole;
+  return writeToConsole
 }
 
-const logger = craeteLogger();
-logger("my name");
-console.log("this is my name");
+const logger = craeteLogger()
+logger('my name')
+console.log('this is my name')
 
 // function as a parameter
 
 const studentsList = Array(500)
   .fill(0)
-  .map((_) => Math.floor(Math.random()) * 10);
+  .map((_) => Math.floor(Math.random()) * 10)
 
-function findUpperHalf(callback) {
+const callback = 'function'
+function findUpperHalf (callback) {
   for (const student of studentsList) {
     if (student >= 5) {
-      console.log(callback);
+      console.log(callback)
     }
   }
 }
-findUpperHalf(callback);
+findUpperHalf(callback)
 
 // Closure
 
-function printSum() {
-  let sum = 0;
+function printSum () {
+  let sum = 0
   findUpperHalf((number) => {
-    sum += number;
-  });
-  logger(sum);
+    sum += number
+  })
+  logger(sum)
 }
 
-printSum();
+printSum()
 
 // FibonacciFunctions
 
 const fib = (n) => {
   // simple manner
-  if (n < 2) return n;
-  return fib(n - 1) + fib(n - 2);
-};
-fib(n);
+  if (n < 2) return n
+  return fib(n - 1) + fib(n - 2)
+}
+fib(n)
 
 const optimizedFib = () => {
-  if (n < 2) return n;
-  const memo = { 0: 0, 1: 1 };
-  const i = 2;
+  if (n < 2) return n
+  const memo = { 0: 0, 1: 1 }
+  const i = 2
   while (i <= 0) {}
-};
-optimizedFib();
+}
+optimizedFib()
 
 // reduce function
 
 const summingFib = () => {
   const sum = studentsList.reduce((acc, number, index, array) => {
     if (number >= 5) {
-      acc.push(number);
+      acc.push(number)
     }
-    return acc;
-  }, 0);
-  logger(sum, "");
-};
-summingFib();
+    return acc
+  }, 0)
+  logger(sum, '')
+}
+summingFib()
